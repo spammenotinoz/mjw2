39422e06e753ff5f3750660bf253e40ef198e9fa
spammenotinoz <spammenotinoz@gmail.com>
2024-09-29 21:20:38 +1000

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { reactive } from 'vue'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(
 
 supabaseUrl,
