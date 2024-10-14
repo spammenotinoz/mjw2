67fc94eba39b050de7224984503b1e93b72f7202
spammenotinoz <spammenotinoz@gmail.com>
2024-10-14 22:44:40 +1100

    Re-Sync

--- src/views/mj/aiFooter.vue
+++ src/views/mj/aiFooter.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 import aiGpts from "./aiGpts.vue"
 import aiGallery from "./aiGallery.vue" 
-<<<<<<< HEAD
-=======
 import realtimeLayout from "@/views/wav/realtimeLayout.vue" 
 
->>>>>>> upstream/main
 </script>
 <template>
 <aiGpts/>
 <aiGallery/>
-<<<<<<< HEAD
-=======
 <!-- <aiOther/> -->
 <realtimeLayout/>
->>>>>>> upstream/main
 </template>
