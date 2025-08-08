df69ca7f1a4758d3c6361a88d204ba58f73cfbe9
spammenotinoz <spammenotinoz@gmail.com>
2025-08-08 21:37:05 +1000

    Update SignIn.vue

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md" style="background-color: black;">
     <form @submit.prevent="handleSignIn" class="animate-in flex w-full flex-col justify-center gap-2">
-      <div class="text-4xl font-bold mb-4 text-white">Midjourney</div>
+      <div class="text-4xl font-bold mb-4 text-white">Ultimate AI Design Studio</div>
       <label for="Email" class="mb-1 text-left text-white">Email</label>
-      <input v-model="email" class="mb-3 rounded-md border bg-inherit px-4 py-2 text-white" type="email" placeholder="you@example.com" required />
+      <input v-model="email" class="mb-3 rounded-md border bg-inherit px-4 py-2 text-white" type="email" placeholder="email@example.com" required />
       <label for="Password" class="mb-1 text-left text-white">Password</label>
-      <input v-model="password" class="mb-6 rounded-md border bg-inherit px-4 py-2 text-white" type="password" placeholder="********" required />
+      <input v-model="password" class="mb-6 rounded-md border bg-inherit px-4 py-2 text-white" type="password" placeholder="**********" required />
       <button type="submit" class="mb-2 rounded-md bg-blue-700 !bg-blue-500 px-4 py-2 text-white" :disabled="loading">
         {{ loading ? 'Signing In...' : 'Sign In' }}
       </button>
